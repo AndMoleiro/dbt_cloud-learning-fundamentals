d78cf794c9db9e2009966b514919adc0db8c5cde
André Moleiro <andre.o.moleiro@gmail.com>
2023-07-16 09:29:17 +0000

    added sources module

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,7 +1,7 @@
 with
     orders as (select * from {{ ref("stg_orders") }}),
 
-    payments as (select * from {{ ref("stg_payments") }}),
+    payments as (select * from {{ ref("stg_payment") }}),
 
     order_payments as (
         select order_id, sum(case when status = 'success' then amount end) as amount
